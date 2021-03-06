f0739562547a5458e41d2e8a9aae837b4bd3de84
Biswas Sampad <biswassampad.official@gmail.com>
2021-03-06 23:10:26 +0530

    test build

---
+++ precache-manifest.0d23721951c2d8030f4a16c46eec2985.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f735519ac9f564cd74d4e5c623ac09cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e3979001d2ceb3d697a",
+    "url": "/static/css/main.1193f659.chunk.css"
+  },
+  {
+    "revision": "338108a07124af96154a",
+    "url": "/static/js/2.f03c5d97.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f03c5d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e3979001d2ceb3d697a",
+    "url": "/static/js/main.6bfd25f3.chunk.js"
+  },
+  {
+    "revision": "44387e0ad0f42443bc07",
+    "url": "/static/js/runtime-main.829db881.js"
+  }
+]);
